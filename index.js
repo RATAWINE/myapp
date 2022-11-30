2a634c5585e34471195f60aec4bf24f382cfee89
Richard Apuliba <apuliba.kojo@yahoo.com>
2022-11-30 10:13:29 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+
+
+app.get("/", (req, res)=>{
+    res.send("<h1>Welcome to azure</h1>")
+})
+
+
+
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log("Server started running...")
+})
